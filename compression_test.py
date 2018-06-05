066f715a8c922cc37f47d4850bc02efccf0dc3c2
Madis Kaspar Nigol <madis1995@gmail.com>
2018-06-05 13:19:24 +0300

    init

---
+++ compression_test.py
@@ -0,0 +1,25 @@
+import cv2
+import numpy as np
+
+def huffman_encode(img):
+    return
+
+def huffman_decode(img):
+    return
+
+def delta_encode(img):
+    encoded = np.zeros_like(img)
+    last = 0
+    for i in range(0, encoded.shape[0]):
+        for j in range(0, encoded.shape[1]):
+            
+
+    return encoded
+
+def delta_decode(img):
+    return
+
+
+
+img = cv2.imread("amazon_gray.png")
+
